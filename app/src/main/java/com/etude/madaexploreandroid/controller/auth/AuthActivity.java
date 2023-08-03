e66dc1b584d933be7f9a2898e78225f0b3cdebc5
Patrick <patrickrasabonihantana2@gmail.com>
2023-08-03 12:22:53 +0200

    init login controller and model

---
+++ app/src/main/java/com/etude/madaexploreandroid/controller/auth/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.etude.madaexploreandroid.controller.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.etude.madaexploreandroid.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
---
+++ app/src/main/java/com/etude/madaexploreandroid/model/User.java
@@ -0,0 +1,8 @@
+package com.etude.madaexploreandroid.model;
+
+public class User {
+    private String roleId;
+    private String firstname;
+    private String lastname;
+    private int state;
+}
